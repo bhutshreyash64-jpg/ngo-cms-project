d2641769577b9e888f3258beb3d22a0440f287f9
Shreyash Bhut <bhutshreyash64@gmail.com>
2026-03-18 19:09:02 +0530

    Day 3: Login functionality implemented

--- ngo_backend/accounts/views.py
+++ ngo_backend/accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/events/')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'accounts/login.html')
