68961dd5ddc67f43b5480260e7617c9f6d2ac446
Shreyash Bhut <bhutshreyash64@gmail.com>
2026-03-19 13:25:39 +0530

    Day 6: Protected routes using login_required

--- ngo_backend/events/views.py
+++ ngo_backend/events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Event
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/login/')
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', {'events': events})
 
 # Create your views here.
